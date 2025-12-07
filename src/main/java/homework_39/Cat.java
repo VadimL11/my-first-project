69d1617f4c95d21c920e938a40cd5b85117a04ff
VadimL11 <dovbnia1994@gmail.com>
2025-12-07 20:49:05 +0100

    Homework lesson 39

---
+++ src/main/java/homework_39/Cat.java
@@ -0,0 +1,15 @@
+package homework_39;
+
+public class Cat {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
