bcfd3db15e8fc84cb3c2c0cf0045467915c7ef5b
VadimL11 <dovbnia1994@gmail.com>
2025-12-17 21:22:16 +0100

    Homework lesson 41

---
+++ src/main/java/homework_41/Student.java
@@ -0,0 +1,17 @@
+package homework_41;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String name;
+    private int age;
+    private double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
